cf2542abea9ca34515521816abd02fcb5d0810e4
JorgeCruz <you@example.com>
2022-11-20 11:51:49 -0600

    carga de notas y ejemplos poo

--- poo.py
+++ poo.py
@@ -1,3 +1,22 @@
-"""
-        
-"""
+## Clase
+class Coche():
+    ## Propiedades
+    largoChasis=250
+    anchoChasis=100
+    ruedas=4
+    enMarcha=False
+    ##Metodos
+    def Arrancar(self):
+        self.enMarcha=True
+    def Estado(self):
+        if(self.enMarcha):
+            return "El coche esta en marcha"
+        else:
+            return "El coche esta detenido"
+
+##Instanciar una clase
+miCoche=Coche()
+
+print("El largo del chasis de miCcoche es: ", miCoche.largoChasis)
+miCoche.Arrancar()
+print(miCoche.Estado())
