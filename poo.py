c66ce9fe20190f35d50e12061763309f66cc68ef
JorgeCruz <you@example.com>
2022-11-22 17:17:20 -0600

    carga de notas y ejemplos poo2

--- poo.py
+++ poo.py
@@ -1,22 +1,44 @@
 ## Clase
 class Coche():
+    ## Constructor
+    def __init__(self):     
     ## Propiedades
-    largoChasis=250
-    anchoChasis=100
-    ruedas=4
-    enMarcha=False
+        self.largoChasis=250
+        self.anchoChasis=100
+        ## Encapsulado de variable
+        self.__ruedas=4
+        self.enMarcha=False
     ##Metodos
-    def Arrancar(self):
-        self.enMarcha=True
-    def Estado(self):
-        if(self.enMarcha):
+    def Arrancar(self, arrancamos):
+        self.enMarcha=arrancamos
+        if(self.__enMarcha):
+            chequeo=self.__chequeo_interno()
+        if (self.__enMarcha and chequeo):
             return "El coche esta en marcha"
+        elif(self.__enMarcha and chequeo==False):
+            return "Algo ha ido mal en el chequeo. No podemos arrancar"
         else:
             return "El coche esta detenido"
 
+    def Estado(self):
+        print("El coche tiene ", self.__ruedas, " ruedas. Unancho de ", self.anchoChasis, " y un largo de ", self.largoChasis)
+
+    ## Encapulado de Metodos
+    def __chequeo_interno(self):
+        print("Realizando chequeo interno")
+        self.gasolina="ok"
+        self.aceite="ok"
+        self.puertas="cerradas"
+        if(self.gasolina=="ok" and self.aceite=="ok" and self.puertas=="cerradas"):
+            return True
+        else:
+            return False
+
 ##Instanciar una clase
 miCoche=Coche()
-
-print("El largo del chasis de miCcoche es: ", miCoche.largoChasis)
-miCoche.Arrancar()
-print(miCoche.Estado())
+print(miCoche.Arrancar(True))
+miCoche.Estado()
+print("------------- A continuacion creamos el segundo objeto-------------------")
+miCoche=Coche()
+print(miCoche.Arrancar(False))
+miCoche.Estado()
